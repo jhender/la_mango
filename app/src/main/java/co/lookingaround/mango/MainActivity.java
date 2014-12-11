3a29a927150553ce6c74666a32bd9b3915cdaecd
Julien <jianhui@gmail.com>
2014-12-11 15:53:31 +0800

    Able to log in and log out, and check profile.

--- app/src/main/java/co/lookingaround/mango/MainActivity.java
+++ app/src/main/java/co/lookingaround/mango/MainActivity.java
@@ -4,14 +4,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.parse.PushService;
 import com.parse.ui.ParseLoginBuilder;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int LOGIN_REQUEST = 0;
+
+    private TextView titleTextView;
+    private TextView emailTextView;
+    private TextView nameTextView;
+    private Button loginOrLogoutButton;
+
+    private ParseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +45,67 @@ public class MainActivity extends ActionBarActivity {
 //        ParseAnalytics.trackAppOpenedInBackground(android.content.Intent);
 //        ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
+
+
         setContentView(R.layout.activity_main);
+
+        titleTextView = (TextView) findViewById(R.id.profile_title);
+        emailTextView = (TextView) findViewById(R.id.profile_email);
+        nameTextView = (TextView) findViewById(R.id.profile_name);
+        loginOrLogoutButton = (Button) findViewById(R.id.login_or_logout_button);
+        titleTextView.setText(R.string.profile_title_logged_in);
+
+        loginOrLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentUser != null) {
+                    // User clicked to log out.
+                    ParseUser.logOut();
+                    currentUser = null;
+                    showProfileLoggedOut();
+                } else {
+                    // User clicked to log in.
+                    ParseLoginBuilder loginBuilder = new ParseLoginBuilder(
+                            MainActivity.this);
+                    startActivityForResult(loginBuilder.build(), LOGIN_REQUEST);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            showProfileLoggedIn();
+        } else {
+            showProfileLoggedOut();
+        }
+    }
+
+    /**
+     * Shows the profile of the given user.
+     */
+    private void showProfileLoggedIn() {
+        titleTextView.setText(R.string.profile_title_logged_in);
+        emailTextView.setText(currentUser.getEmail());
+        String fullName = currentUser.getString("name");
+        if (fullName != null) {
+            nameTextView.setText(fullName);
+        }
+        loginOrLogoutButton.setText(R.string.profile_logout_button_label);
+    }
+
+    /**
+     * Show a message asking the user to log in, toggle login/logout button text.
+     */
+    private void showProfileLoggedOut() {
+        titleTextView.setText(R.string.profile_title_logged_out);
+        emailTextView.setText("");
+        nameTextView.setText("");
+        loginOrLogoutButton.setText(R.string.profile_login_button_label);
     }
 
 
